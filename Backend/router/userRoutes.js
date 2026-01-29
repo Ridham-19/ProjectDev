431b17a9550ec9a856dbc6364a8b9a629abe025d
Ridham-19 <ridhamkamboj90@gmail.com>
2026-01-29 22:04:27 +0530

    handled the logout of the user

--- Backend/router/userRoutes.js
+++ Backend/router/userRoutes.js
@@ -8,14 +8,15 @@ import {
     resetPassword
 } from "../controllers/authController.js";
 import multer from "multer";
+import { isAuthenticated } from "../middlewares/authMiddleware.js";
 
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", login);
-router.get("/me", getUser);
-router.get("/logout", logout);
+router.get("/logout",isAuthenticated, logout);
+router.get("/me",isAuthenticated, getUser);
 router.post("/password/forgot", forgotPassword);
 router.put("/password/reset/:token", resetPassword);
 
